cf9eed9147f4a3d3b4333ffdb1ac5c8d674acb8d
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-09-24 22:03:14 -0300

    Commit Fix Notes

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Formulario from "./components/Form";
+import ListaDeNotas from "./components/ListNotas";
+import "./assets/App.css";
+import './assets/index.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      notas: []
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
+  render(){
+    return (
+      <section className="conteudo">
+        <Formulario criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
+      </section>
+    );
+  }
+}
+
+export default App;
